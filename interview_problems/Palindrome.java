6749e0f50f991d63a00dd5f376e3946b69bce15d
natkofatko <natalialal2325@gmail.com>
2019-02-14 15:04:29 -0500

    Add files via upload

--- interview_problems/Palindrome.java
+++ interview_problems/Palindrome.java
@@ -10,6 +10,11 @@ public class Palindrome {
 		System.out.println(palindrome1("HEeh"));		
 		
 		System.out.println(palindrome2("HEleh"));
+		System.out.println(isP("Hehe"));
+		System.out.println(isPal4("Mama"));
+		
+		
+		System.out.println(match(1221));
 	}
 	
 	public static boolean palindrome2(String s)
@@ -49,5 +54,59 @@ public class Palindrome {
 		
 	}
 	
+	//using recursice call
+	public static boolean isPalRec(String str, int s, int e)
+	{
+		if(s==e)
+			return true; 
+		if(str.charAt(s)!=str.charAt(e))
+return false; 
+		if (s<e+1)
+			return isPalRec(str, s+1, e-1);
+		return true; 
+		
+	}
 	
+	public static boolean isP(String s)
+	{
+		int n = s.length();
+		if (n==0)
+			return true; 
+		return isPalRec(s, 0, n-1); 
+	}
+	
+	//using reverse library
+	public static boolean isPal4(String s)
+	{
+		String reverse = new StringBuffer(s).reverse().toString(); 
+		if(s.equalsIgnoreCase(reverse))
+			return true; 
+		else return false; 
+	}
+	
+	public static int isNumPalindrome(int n)
+	{
+		int reminder=0;
+		int reversed =0; 
+		
+		while(n>0)
+		{
+			reminder = n%10;
+			n=n/10; 
+			reversed = reversed*10 +reminder; 
+		}
+		
+	 return reversed; 
+		
+		
+		
+	
+		
+	}
+	public static boolean match(int num)
+	{
+		if(num==isNumPalindrome(num))
+		return true;
+		else return false; 
+	}
 }
